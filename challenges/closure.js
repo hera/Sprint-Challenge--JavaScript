3a7171ca082dd35cb0f06c0f75339be8ddf6ee06
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-04-24 09:39:21 -0700

    Do tasks on closures

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,28 @@ myFunction();
 
 // Explanation: 
 
+/*
+
+nestedFunction scope was created in myFunction scope.
+javascript first looked for the variable 'internal' in nestedFunction scope, but couldn't find it
+so it looked in the outer scope of myFunction and found it there.
+
+*/
+
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
+function summation(num) {
+    let result = 0;
+
+    for (let counter = 1; counter <= num; counter++) {
+        result += counter;
+    }
+
+    return result;
+}
+
+console.log(summation(4)); // 10
